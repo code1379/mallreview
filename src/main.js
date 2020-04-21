472fd12036a0a989205b118b4ba5f5f962fbae12
ayiooo <coderxialuo@gmail.com>
2020-04-21 11:56:29 +0800

    初始化项目 html的viewport、stylus+iconfont、webpack路径别名、在main.js中引入stylus

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入样式文件
+import "assets/stylus/index.styl"
 
 Vue.config.productionTip = false
 
