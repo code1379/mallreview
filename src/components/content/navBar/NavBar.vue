f0074119a66daa558c8255016e39623813ef04ee
ayiooo <coderxialuo@gmail.com>
2020-04-21 20:27:08 +0800

    添加navbar

---
+++ src/components/content/navBar/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar">
+    <div
+      class="item"
+      v-for="(title, index) in titles"
+      :key="title"
+      :class="{active: index === this.currentIndex}"
+    >
+      <span class="title">{{title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return ["流行", "新款", "经典"];
+      }
+    },
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.navbar
+  line-height: 40px
+  display: flex
+  .item
+    flex: 1
+    line-height: 20px
+    // .title
+</style>
